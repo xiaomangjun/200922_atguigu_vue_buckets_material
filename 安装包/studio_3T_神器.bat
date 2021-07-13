0a8bbf53dea7b693c8ad8fe3b3fa8fe6978e3b79
下一站守候 <3192477509@qq.com>
2021-07-13 18:59:09 +0800

    atguigu_vue_buckets_material

---
+++ 安装包/studio_3T_神器.bat
@@ -0,0 +1,8 @@
+﻿@echo off
+ECHO ok!!!!!!!!!
+FOR /f "tokens=1,2,* " %%i IN ('reg query "HKEY_CURRENT_USER\Software\JavaSoft\Prefs\3t\mongochef\enterprise" ^| find /V "installation" ^| find /V "HKEY"') DO ECHO yes | reg add "HKEY_CURRENT_USER\Software\JavaSoft\Prefs\3t\mongochef\enterprise" /v %%i /t REG_SZ /d ""
+
+ECHO ok!!!!!!!!!
+epause>nul
+
+exit
